2edef621355bfe2f3058f6d571ff234870a9c153
Tina-Matrix <610363561@QQ.COM>
2026-06-21 13:41:20 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "saving-planner-v6";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./supabase-config.js?v=7"];
+const CACHE_NAME = "saving-planner-v7";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./supabase-config.js?v=8"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
